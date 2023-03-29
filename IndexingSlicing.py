f5284454cfa3414f4ec82943fbeedddb74b09e80
lindsay <jylindsay0523@gmail.com>
2023-03-30 00:01:16 +0900

    1 Python Basic: Indexing, Slicing

---
+++ IndexingSlicing.py
@@ -0,0 +1,13 @@
+x= "Rome is not built in a day!"
+#Indexing
+print(x[0])
+print(x[-1])
+print(x[-6])
+print(x[12])
+
+#Slicing
+print(x[0:4])
+print(x[12:])
+print(x[:7])
+print(x[:])
+print(x[12:-7])
